e11976b338b98dea5a957552f21fab04057a56fd
Jimmy Karlsson <jk224jv@student.lnu.se>
2024-02-27 17:42:21 +0100

    Extend the request object TS-propper

---
+++ src/lib/types/req-extentions.d.ts
@@ -0,0 +1,18 @@
+/**
+ * Extends the request object and Gives the request object a new properties.
+ * @property {string} requestUuid - A unique identifier for the request.
+ * @property {string} code - The current request's session ID towards gitlab, if available.
+ * @property {string} token - The current request's token towards gitlab, if available.
+ * @property {string} refreshToken - The current request's refresh token towards gitlab, if available.
+ * @module ExtendedRequest
+ */
+import 'express-serve-static-core';
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    requestUuid?: string
+    code?: string
+    token?: string
+    refreshToken?: string
+  }
+}
