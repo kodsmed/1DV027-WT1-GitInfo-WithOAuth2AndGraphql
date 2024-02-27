73ba9067f28f45c5694c09cf3db8442ab29ba6c8
Jimmy Karlsson <jk224jv@student.lnu.se>
2024-02-27 17:45:29 +0100

    prepare for IoC

--- src/server.ts
+++ src/server.ts
@@ -1,74 +1,137 @@
-import express from 'express';
+/**
+ * @file This file is the main entry point for the application.
+ * @description sets up the IoC container, sets up and starts the Express Server.
+ * @module server
+ * @author Jimmy Karlsson <jk224jv@student.lnu.se>
+ */
+
+// WARNING: Package author recommend this to be first to avoid issues with async.
+// WARNING: This package does not go well with express-jwt.
+import httpContext from 'express-http-context';
+
+// Node built-in modules.
+import { randomUUID } from 'node:crypto'
+import http from 'node:http'
+
+// Package modules.
+import helmet from 'helmet';
+import cors from 'cors';
+import express, { NextFunction, Request, Response } from 'express';
 import 'dotenv/config'
+import '@lnu/json-js-cycle'
+import { morganLogger } from './config/morgan.js';
+
+// Application modules.
+import { limiter } from './config/rateLimiter.js'
+import { logger } from './config/winston.js'
 import { router } from './routes/router.js'
 
-const port = process.env.EXPRESS_PORT;
-const applicationID = process.env.GITLAB_CLIENT_ID;
-const applicationSecret = process.env.GITLAB_CLIENT_SECRET;
-const host = process.env.GITLAB_HOST_URL;
-if (!port) {
-  throw new Error('EXPRESS_PORT is not set');
+// Load extended types.
+import ExtendedRequest from './lib/types/req-extentions.js';
+import { ExtendedError } from './lib/types/ExtendedError.js';
+
+// Set up
+try {
+  const app = express();
+  const port = process.env.EXPRESS_PORT;
+  const applicationID = process.env.GITLAB_CLIENT_ID;
+  const applicationSecret = process.env.GITLAB_CLIENT_SECRET;
+  const host = process.env.GITLAB_HOST_URL;
+  const callbackURL = `http://localhost:${port}/oauth-callback/`;
+  const gitLabAuthURL = `${host}/oauth/authorize`;
+
+  if (!port) {
+    throw new Error('EXPRESS_PORT is not set');
+  }
+  if (!applicationID) {
+    throw new Error('GITLAB_CLIENT_ID is not set');
+  }
+  if (!applicationSecret) {
+    throw new Error('GITLAB_CLIENT_SECRET is not set');
+  }
+  if (!host) {
+    throw new Error('GITLAB_HOST_URL is not set');
+  }
+
+  // Set various HTTP headers to make the application little more secure (https://www.npmjs.com/package/helmet).
+  app.use(helmet())
+
+  // Enable Cross Origin Resource Sharing (CORS) (https://www.npmjs.com/package/cors).
+  app.use(cors())
+
+  // Parse requests of the content type application/json.
+  app.use(express.json())
+
+  // Add the request-scoped context.
+  // NOTE! Must be placed before any middle that needs access to the context!
+  //       See https://www.npmjs.com/package/express-http-context.
+  app.use(httpContext.middleware)
+
+
+  // Use a morgan logger.
+  app.use(morganLogger)
+
+  // Apply the rate limiting middleware to all requests.
+  app.use(limiter)
+
+  // Middleware to be executed before the routes.
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    // Add a request UUID to each request and store information about
+    // each request in the request-scoped context.
+    req.requestUuid = randomUUID()
+    httpContext.set('request', req)
+
+    next()
+  })
+
+  // Register routes.
+  app.use('/', router)
+
+  // Error handler.
+  app.use((err: ExtendedError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message, { error: err })
+
+    if (process.env.NODE_ENV === 'production') {
+      // Ensure a valid status code is set for the error.
+      // If the status code is not provided, default to 500 (Internal Server Error).
+      // This prevents leakage of sensitive error details to the client.
+      if (!err.status) {
+        err.status = 500
+        err.message = http.STATUS_CODES[err.status] || 'Internal Server Error'
+      }
+
+      // Send only the error message and status code to prevent leakage of
+      // sensitive information.
+      res
+        .status(err.status)
+        .json({
+          error: err.message
+        })
+
+      return
+    }
+
+    // ---------------------------------------------------
+    // ⚠️ WARNING: Development Environment Only!
+    //             Detailed error information is provided.
+    // ---------------------------------------------------
+
+    // Deep copies the error object and returns a new object with
+    // enumerable and non-enumerable properties (cyclical structures are handled).
+    const copy = JSON.decycle(err, { includeNonEnumerableProperties: true })
+
+    return res
+      .status(err.status || 500)
+      .json(copy)
+  })
+
+  // Starts the HTTP server listening for connections.
+  const server = app.listen(port, () => {
+    logger.info(`Server running at http://localhost:${port}`)
+    logger.info('Press Ctrl-C to terminate...')
+  })
+} catch (err) {
+  const errorString = err instanceof Error ? err.message : 'An unknown error occurred'
+  logger.error(errorString, { error: err })
+  process.exitCode = 1
 }
-if (!applicationID) {
-  throw new Error('GITLAB_CLIENT_ID is not set');
-}
-if (!applicationSecret) {
-  throw new Error('GITLAB_CLIENT_SECRET is not set');
-}
-if (!host) {
-  throw new Error('GITLAB_HOST_URL is not set');
-}
-
-const callbackURL = `http://localhost:${port}/oauth-callback/`;
-const gitLabAuthURL = `${host}/oauth/authorize`;
-
-
-console.log('EXPRESS_PORT: ' + process.env.EXPRESS_PORT)
-const app = express();
-
-// Register routes.
-app.use('/', router)
-
-// app.get('/auth/gitlab', (req, res) => {
-//   const authURL = `${gitLabAuthURL}?client_id=${applicationID}&redirect_uri=${callbackURL}&response_type=code&scope=read_user%20read_api`;
-//   res.redirect(authURL);
-// });
-
-// app.get('/oauth-callback', async (req, res) => {
-//   const { code } = req.query;
-//   console.log('code:', code)
-
-//   try {
-//     const tokenResponse = await fetch(`${host}/oauth/token`, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({
-//         client_id: applicationID,
-//         client_secret: applicationSecret,
-//         code,
-//         grant_type: 'authorization_code',
-//         redirect_uri: callbackURL,
-//       })
-//     });
-
-//     const tokenData = await tokenResponse.json();
-//     const accessToken = tokenData.access_token;
-//     const refreshToken = tokenData.refresh_token;
-//     const expiresIn = tokenData.expires_in;
-//     console.log('tokenData:', tokenData)
-//     console.log('accessToken:', accessToken)
-//     console.log('refreshToken:', refreshToken)
-//     console.log('expiresIn:', expiresIn)
-//     // Here you can use the access token to make authenticated requests to GitLab
-//     res.send('Authentication successful!');
-//   } catch (error) {
-//     console.error('Error exchanging code for token', error);
-//     res.status(500).send('Authentication failed');
-//   }
-// });
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
