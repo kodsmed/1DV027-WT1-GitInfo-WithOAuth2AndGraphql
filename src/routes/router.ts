a1e8aacb313415ba0ec00b5501609e5e0e70c3bf
Jimmy Karlsson <jk224jv@student.lnu.se>
2024-02-28 13:01:11 +0100

    add experimental session handling

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -8,6 +8,11 @@
 import express from 'express'
 
 import { authRouter } from './auth/authRouter.js'
+import { Cookie } from 'express-session'
+import ExtendedRequest from '../lib/types/req-extentions.js'
+
+// import custom types
+import SessionData from '../lib/types/sessionData-extentions.js'
 
 /**
  * The main router for the application
@@ -19,6 +24,20 @@ router.get('/', (req, res) => {
 })
 
 router.use('/auth', authRouter)
+
+router.get('/cookie-set', (req, res) => {
+  req.session.UUID = req.requestUuid;
+  res.send('Cookie set');
+})
+
+router.get('/cookie-get', (req, res) => {
+  // Replace 'cookieName' with the actual name of the cookie you want to retrieve
+  if (req.session.UUID) {
+    res.send('Welcome to your dashboard! \n' + req.session.UUID);
+  } else {
+    res.send('Please login to view your dashboard');
+  }
+})
 
 router.use('*', (req, res, next) => {
   next(new Error('404 - Route not found'))
