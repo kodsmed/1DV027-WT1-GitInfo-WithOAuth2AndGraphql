ca31e3ca7a14029b51734d7313ff8528d86274d7
Jimmy Karlsson <jk224jv@student.lnu.se>
2024-02-29 18:56:03 +0100

    soften SessionOptions

--- src/config/sessionOptions.ts
+++ src/config/sessionOptions.ts
@@ -7,17 +7,17 @@ import { SessionOptions } from 'express-session'
 if (!process.env.SESSION_SECRET) {
   throw new Error('SESSION_SECRET is not set')
 }
-
+const isProduction = process.env.NODE_ENV === 'production' ? true : false
 export const sessionOptions: SessionOptions= {
-  name: 'WT1',
+  name: 'WT1-2',
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
+    secure: false,
     httpOnly: true,
-    sameSite: 'strict',
+    sameSite: 'lax',
     // one hour
-    maxAge: 1000 * 60 * 60,
+    maxAge: 1000 * 60 * 60 * 24,
   },
 }
